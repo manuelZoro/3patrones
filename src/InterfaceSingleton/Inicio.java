239b9ea61b7f1ae250f45b4a15fc951a8249d1eb
Manuel Clarke <manuel@Zoro.(none)>
2014-10-26 17:55:59 -0600

    full

--- src/InterfaceSingleton/Inicio.java
+++ src/InterfaceSingleton/Inicio.java
@@ -3,11 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Interface;
-
-import PizzasOaxaca.FabricaPizza;
-import PizzasOaxaca.HUAPizzaSucursal;
-import PizzasOaxaca.OAXPizzaSucursal;
+package InterfaceSingleton;
 
 /**
  *
@@ -20,8 +16,9 @@ public class Inicio extends javax.swing.JFrame {
      */
     public Inicio() {
         initComponents();
-        setSize(649,400);
+        setSize(850,440);
         setResizable(false);
+        setTitle("Bienvenido!!");
     }
 
     /**
@@ -37,49 +34,61 @@ public class Inicio extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Verdana", 3, 36)); // NOI18N
-        jLabel1.setText("Bienvenido a la Pizzeria ");
+        jLabel1.setForeground(new java.awt.Color(254, 254, 254));
+        jLabel1.setText("Bienvenido a la Pizzeria Planeta ");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(90, 0, 500, 80);
+        jLabel1.setBounds(140, 10, 660, 80);
 
+        jButton1.setForeground(new java.awt.Color(1, 1, 1));
         jButton1.setText("Oaxaca");
+        jButton1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
         getContentPane().add(jButton1);
-        jButton1.setBounds(350, 160, 230, 150);
+        jButton1.setBounds(60, 300, 140, 80);
 
+        jButton2.setBackground(new java.awt.Color(254, 254, 254));
+        jButton2.setForeground(new java.awt.Color(1, 1, 1));
         jButton2.setText("Huajuapan");
+        jButton2.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
         getContentPane().add(jButton2);
-        jButton2.setBounds(90, 160, 230, 150);
+        jButton2.setBounds(60, 160, 140, 80);
 
+        jLabel2.setFont(new java.awt.Font("Verdana", 3, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(254, 254, 254));
         jLabel2.setText("Selecciona una sucursal");
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(240, 90, 170, 40);
+        jLabel2.setBounds(40, 60, 190, 60);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InterfaceSingleton/Pizza_Planet2.jpg"))); // NOI18N
+        jLabel3.setText("jLabel3");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(0, 0, 880, 430);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        FabricaPizza Huajuapan= new HUAPizzaSucursal();
-        new Principal(Huajuapan).setVisible(true);
+        Principal ventana = Principal.getInstance(1);
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        FabricaPizza Oaxaca= new OAXPizzaSucursal();
-        new Principal(Oaxaca).setVisible(true);
+        Principal ventana = Principal.getInstance(2);
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -124,5 +133,6 @@ public class Inicio extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
